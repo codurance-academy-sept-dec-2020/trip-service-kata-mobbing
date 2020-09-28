bc6d19c3d93234217737acc0051991f0014378eb
Fabio D'Amico <fabio.damico@codurance.com>
2020-09-28 11:08:47 +0100

    :recycle: Refactoring code using one single stub in place of 3 stub

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -1,29 +1,57 @@
 package org.craftedsw.tripservicekata.trip;
 
-import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
-import org.craftedsw.tripservicekata.trip.helpers.NotLoggedUserTripService;
-import org.craftedsw.tripservicekata.user.User;
-import org.junit.jupiter.api.Test;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.Test;
 
 public class TripServiceShould {
 
   @Test
   void tell_a_user_is_not_logged_in() {
-    TripService tripService = new NotLoggedUserTripService();
+    TripService tripService = new StubTripService(null);
     User user = new User();
     assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(user));
   }
 
   @Test
   void tell_a_logged_user_has_no_trips() {
-    TripService tripService = new LoggedUserTripService();
     User user = new User();
+    TripService tripService = new StubTripService(user);
     assertThat(tripService.getTripsByUser(user), empty());
+  }
+
+  @Test
+  void tell_a_logged_user_has_trips() {
+    User user = new User();
+    user.addTrip(new Trip());
+    user.addFriend(user);
+    TripService tripService = new StubTripService(user);
+    assertThat(tripService.getTripsByUser(user), hasSize(1));
+  }
+
+  public class StubTripService extends TripService {
+
+    private final User user;
+
+    public StubTripService(User user) {
+      this.user = user;
+    }
+
+    @Override
+    protected List<Trip> getUserTrips(User user) {
+      return user.trips();
+    }
+
+    @Override
+    protected User getLoggedUser() {
+      return user;
+    }
   }
 
 }
