ac59f9d32bee4d28160a0c4ea57866372d4f76dc
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-09-28 11:19:02 +0200

    Reformatting code.

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -9,28 +9,28 @@ import org.craftedsw.tripservicekata.user.UserSession;
 
 public class TripService {
 
-	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = getLoggedUser();
-		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = TripDAO.findTripsByUser(user);
-			}
-			return tripList;
-		} else {
-			throw new UserNotLoggedInException();
-		}
-	}
+  public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
+    List<Trip> tripList = new ArrayList<Trip>();
+    User loggedUser = getLoggedUser();
+    boolean isFriend = false;
+    if (loggedUser != null) {
+      for (User friend : user.getFriends()) {
+        if (friend.equals(loggedUser)) {
+          isFriend = true;
+          break;
+        }
+      }
+      if (isFriend) {
+        tripList = TripDAO.findTripsByUser(user);
+      }
+      return tripList;
+    } else {
+      throw new UserNotLoggedInException();
+    }
+  }
 
-	protected User getLoggedUser() {
-		return UserSession.getInstance().getLoggedUser();
-	}
+  protected User getLoggedUser() {
+    return UserSession.getInstance().getLoggedUser();
+  }
 
 }
--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -7,18 +7,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TripServiceTest {
-    @Test
-    void user_is_not_logged_should_throws_an_exception() {
-        TripService tripService = new FakeTripService();
-        User user = new User();
-        assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(user));
-    }
+
+  @Test
+  void user_is_not_logged_should_throws_an_exception() {
+    TripService tripService = new FakeTripService();
+    User user = new User();
+    assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(user));
+  }
 
 
-    class FakeTripService extends TripService {
-        public User getLoggedUser() {
-            return null;
-        }
+  class FakeTripService extends TripService {
+
+    public User getLoggedUser() {
+      return null;
     }
+  }
 }
 
