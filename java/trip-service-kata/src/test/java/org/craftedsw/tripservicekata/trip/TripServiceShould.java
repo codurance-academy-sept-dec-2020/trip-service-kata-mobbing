7cd694bad7ffa6bce4b16a8c6d556b1f4723b89b
Fabio D'Amico <fabio.damico@codurance.com>
2020-09-28 13:55:58 +0100

    :recycle: Refactoring ours tub with a constructor in place of a builder

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -5,6 +5,7 @@ import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Arrays;
 import java.util.List;
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
@@ -14,33 +15,45 @@ public class TripServiceShould {
 
   @Test
   void tell_a_user_is_not_logged_in() {
-    TripService tripService = new StubTripService(null);
-    User user = new User();
-    assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(user));
+    TripService tripService = new StubTripService(null, null);
+    assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(new User()));
   }
 
   @Test
   void tell_a_logged_user_has_no_trips() {
-    User user = new User();
-    TripService tripService = new StubTripService(user);
-    assertThat(tripService.getTripsByUser(user), empty());
+    User loggedUser = new User();
+    User friendsUser = new User();
+    User[] friends = new User[]{loggedUser};
+    TripService tripService = new StubTripService(loggedUser, friendsUser, friends, new Trip[]{});
+    assertThat(tripService.getTripsByUser(friendsUser), empty());
   }
 
   @Test
   void tell_a_logged_user_has_trips() {
-    User user = new User();
-    user.addTrip(new Trip());
-    user.addFriend(user);
-    TripService tripService = new StubTripService(user);
-    assertThat(tripService.getTripsByUser(user), hasSize(1));
+    User loggedUser = new User();
+    User friendsUser = new User();
+    User[] friends = new User[]{loggedUser};
+    Trip[] trips = new Trip[]{new Trip()};
+
+    TripService tripService = new StubTripService(loggedUser, friendsUser, friends, trips);
+    assertThat(tripService.getTripsByUser(friendsUser), hasSize(1));
   }
 
   public class StubTripService extends TripService {
 
-    private final User user;
+    private final User loggedUser;
+    private final User friendsUser;
 
-    public StubTripService(User user) {
-      this.user = user;
+    public StubTripService(User loggedUser, User friendsUser, User[] friends, Trip[] trips) {
+      this.loggedUser = loggedUser;
+      this.friendsUser = friendsUser;
+      Arrays.stream(friends).forEach(this.friendsUser::addFriend);
+      Arrays.stream(trips).forEach(this.friendsUser::addTrip);
+    }
+
+    public StubTripService(User loggedUser, User otherUser) {
+      this.loggedUser = loggedUser;
+      this.friendsUser = otherUser;
     }
 
     @Override
@@ -50,7 +63,7 @@ public class TripServiceShould {
 
     @Override
     protected User getLoggedUser() {
-      return user;
+      return loggedUser;
     }
   }
 
