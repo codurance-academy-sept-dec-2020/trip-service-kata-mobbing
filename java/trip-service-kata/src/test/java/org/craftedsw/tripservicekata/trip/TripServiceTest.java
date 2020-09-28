e0484f7ad5b362f372e70333d7caa9cf2985d5bd
martin-garcia-blanco <programadopormartin@gmail.com>
2020-09-28 11:13:55 +0200

    :sparkles: Create test when user is not logged mocking the class

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,24 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class TripServiceTest {
-	
+    @Test
+    void user_is_not_logged_should_throws_an_exception() {
+        TripService tripService = new FakeTripService();
+                User user = new User();
+        assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(user));
+    }
+
+
+    class FakeTripService extends TripService {
+        public User getLoggedUser() {
+            return null;
+        }
+    }
 }
+
