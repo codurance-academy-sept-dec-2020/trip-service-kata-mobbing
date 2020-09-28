d2ac59a3d3dfed5f276df9a91720258cb2605526
Fabio D'Amico <fabio.damico@codurance.com>
2020-09-28 14:43:26 +0100

    :recycle: Refactoring using builder in place of previous constructor

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -5,56 +5,55 @@ import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.Arrays;
 import java.util.List;
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TripServiceShould {
 
+  private final static User REGISTERED_USER = new User();
+  private final static User GUEST_USER = null;
+  private final static User ANOTHER_USER = new User();
+  private final static Trip TRIP_TO_ROME = new Trip();
+  private TripService tripService;
+  private User loggedInUser;
+
+  @BeforeEach
+  void setUp() {
+    tripService = new StubTripService();
+    loggedInUser = GUEST_USER;
+  }
+
   @Test
   void tell_a_user_is_not_logged_in() {
-    TripService tripService = new StubTripService(null, null);
-    assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(new User()));
+    loggedInUser = GUEST_USER;
+    assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(ANOTHER_USER));
   }
 
   @Test
   void tell_a_logged_user_has_no_trips() {
-    User loggedUser = new User();
-    User friendsUser = new User();
-    User[] friends = new User[]{loggedUser};
-    TripService tripService = new StubTripService(loggedUser, friendsUser, friends, new Trip[]{});
-    assertThat(tripService.getTripsByUser(friendsUser), empty());
+    loggedInUser = REGISTERED_USER;
+    User aUser = UserBuilder.aUser()
+        .withFriends(loggedInUser)
+        .build();
+
+    assertThat(tripService.getTripsByUser(aUser), empty());
   }
 
   @Test
   void tell_a_logged_user_has_trips() {
-    User loggedUser = new User();
-    User friendsUser = new User();
-    User[] friends = new User[]{loggedUser};
-    Trip[] trips = new Trip[]{new Trip()};
+    loggedInUser = REGISTERED_USER;
+    User aUser = UserBuilder.aUser()
+        .withFriends(loggedInUser)
+        .withTrips(TRIP_TO_ROME)
+        .build();
 
-    TripService tripService = new StubTripService(loggedUser, friendsUser, friends, trips);
-    assertThat(tripService.getTripsByUser(friendsUser), hasSize(1));
+    assertThat(tripService.getTripsByUser(aUser), hasSize(1));
   }
 
   public class StubTripService extends TripService {
-
-    private final User loggedUser;
-    private final User friendsUser;
-
-    public StubTripService(User loggedUser, User friendsUser, User[] friends, Trip[] trips) {
-      this.loggedUser = loggedUser;
-      this.friendsUser = friendsUser;
-      Arrays.stream(friends).forEach(this.friendsUser::addFriend);
-      Arrays.stream(trips).forEach(this.friendsUser::addTrip);
-    }
-
-    public StubTripService(User loggedUser, User otherUser) {
-      this.loggedUser = loggedUser;
-      this.friendsUser = otherUser;
-    }
 
     @Override
     protected List<Trip> getUserTrips(User user) {
@@ -63,9 +62,44 @@ public class TripServiceShould {
 
     @Override
     protected User getLoggedUser() {
-      return loggedUser;
+      return loggedInUser;
     }
   }
 
+  private static class UserBuilder {
+
+    private User[] users;
+    private Trip[] trips;
+
+    public UserBuilder() {
+      this.users = new User[]{};
+      this.trips = new Trip[]{};
+    }
+
+    public static UserBuilder aUser() {
+      return new UserBuilder();
+    }
+
+    public UserBuilder withFriends(User... users) {
+      this.users = users;
+      return this;
+    }
+
+    public UserBuilder withTrips(Trip... trips) {
+      this.trips = trips;
+      return this;
+    }
+
+    public User build() {
+      User aUser = new User();
+      for (User user : users) {
+        aUser.addFriend(user);
+      }
+      for (Trip trip : trips) {
+        aUser.addTrip(trip);
+      }
+      return aUser;
+    }
+  }
 }
 
